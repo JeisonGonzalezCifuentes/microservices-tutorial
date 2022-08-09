431031942236c2e613438878b5af0a9d47db2cb2
jeisongonzalez <jgonzalez@modyo.com>
2022-08-09 13:14:48 -0500

    Adding gateway configuration

--- user-service/src/main/java/com/user/service/feign/clients/CarFeignClient.java
+++ user-service/src/main/java/com/user/service/feign/clients/CarFeignClient.java
@@ -1,13 +1,15 @@
 package com.user.service.feign.clients;
 
-import com.user.service.models.Bike;
 import com.user.service.models.Car;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "car-service", url = "http://localhost:8082/car")
+@FeignClient(name = "car-service")
 public interface CarFeignClient {
 
     @PostMapping()
